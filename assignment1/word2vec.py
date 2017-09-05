06b1bcfc69b1014d555edda17b5177418dcc2f80
Jigar Gandhi <jigarr.gandhi@gmail.com>
2017-09-05 18:44:08 +0530

    Word 2Vec: Negative sampling loss

--- assignment1/word2vec.py
+++ assignment1/word2vec.py
@@ -14,7 +14,10 @@ class Word2Vec:
         self.window = window
         self.tokensize = self.preprocess()
         self.V = np.random.rand(self.tokensize, d)
-        self.U = np.random.rand(self.tokensize, d) 
+        self.b1 = np.random.rand(self.tokensize,d)
+        self.U = np.random.rand(self.tokensize, d)
+        self.b2 = np.random.rand(self.tokensize,d)
+        self.rate = 0.005
         
         
     def preprocess(self):
@@ -41,13 +44,14 @@ class Word2Vec:
             val = np.array((2*self.window, self.tokensize), dtype= float)
             arr = [i for i in range(index-self.window, index + self.window +1) if i != index]
         
-        print(arr)
+        outputVectors =None
         for idx, val in enumerate(arr):
-            # create a np array of row vectors of output
-            print(self.getVector(self.words[val]).shape)
-        return val
-                    
-        pass
+            if idx ==0:
+                outputVectors = self.getVector(self.words[val])
+            else:
+                #find a better way to do it
+                outputVectors = np.concatenate((outputVectors,self.getVector(self.words[val])),axis=0)
+        return outputVectors
     
     def getVector(self, word):
         index =self.tokens.index(word)
@@ -55,11 +59,41 @@ class Word2Vec:
         x[index] = 1
         return x.T
     
-    def softmaxCostAndGradient(self, predicted, target, outputVectors):
-        pass
+    def softmaxCostAndGradient(self, idx, outputVectors, negativeLabels=[]):
+        cost=None
+        gradU = None
+        gradV = None
+        
+        #feedforward
+        vc= self.V[idx,:]
+        uo= self.U[idx,:]
+        #http://kb.timniven.com/?p=181
+        sigmoid_uo_vc =sigmoid(uo.T.dot(vc)) 
+        del_vc_1 = (1-sigmoid_uo_vc)*uo
+        del_vc_2 = np.zeros_like(del_vc_1)
+        del_uc_1 = (sigmoid_uo_vc-1)*vc
+        del_uc_2 = np.zeros_like(del_vc_1)
+        secondCost=0
+        for i in negativeLabels:
+            uk = self.U[i,:]
+            sigm= sigmoid(-1*uk.T.dot(vc))
+            secondCost += np.log(sigm)
+            del_vc_2 += uk*(sigm+1)
+            del_uc_2+= -1*(sigm-1)*vc
+        
+        del_vc = del_vc_1+ del_vc_2
+        del_uc = del_uc_1+ del_uc_2
+        cost = 0
+        cost += np.log(sigmoid_uo_vc)+ secondCost       
+        
+        return cost, del_vc, del_uc
+        
     
-    def sgd():
-        pass
+    def sgd(self):
+        for idx, val in enumerate(self.words):
+            cost, gradU, gradV = self.softmaxCostAndGradient(idx, self.getWindowVectors(idx))
+            self.U = self.U + self.rate* gradU
+            self.V = self.V +self.rate*gradV
     
     
 
